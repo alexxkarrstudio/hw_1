f8c1e9d6bef8103dd2378f3e4c82497db63bc5bf
“Alex” <alexxkarrstudio@gmail.com>
2020-08-31 10:49:37 -0500

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+// let numberOfFilms = prompt('How many movies did you watch?', '12');
+
+// console.log(numberOfFilms);
+
+// const box = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// console.log(box);
+
+const answears = [];
+
+answears[0] = prompt('Last movie?', '');
+answears[1] = prompt('Your score', '');
+
+
+let movies = {
+    0: answears[0], 
+    1: [1]
+};
+
+
+console.log(answears);
